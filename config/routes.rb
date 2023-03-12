97f1f6494425744a6cc27bddcbab45fdfd63374e
Yuchi Hsueh <bhsueh94@gmail.com>
2023-03-12 16:27:23 -0300

    adds gems google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
